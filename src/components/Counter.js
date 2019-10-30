3bb41444e4b3ba703a4f6a799a624a1be29574a5
unknown <phuocduc2012@gmail.com>
2019-10-30 16:58:54 +0700

    let go

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {connect} from 'redux'
+
+// function mapStateToProps(state,ownProps) 
+
+export const Counter() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// const mapStateToProps = ''
+// const mapStateToProps = ''
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
